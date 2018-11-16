1723525aa874e87876cfb192a7395cd72cc67dea
CoryWritesCode <corycunningham2010@gmail.com>
2018-11-15 23:34:19 -0600

    advanced

---
+++ popular-downloader.js
@@ -0,0 +1,19 @@
+const rp = require('request-promise');
+const path = require('path');
+const fs = require('fs');
+
+let downloadPath = path.join(__dirname, './popular-articles.json');
+
+rp('https://reddit.com/r/popular.json')
+  .then(res => {
+    JSON.parse(res).data.children.forEach(item => {
+      if (item.data.is_video == true) {
+        fs.writeFile(`./downloads/${item.data.title}`, item.data.fallback_url);
+      } else if (path.extname(item.data.url) == '.jpg') {
+        fs.writeFile(`./downloads/${item.data.title}`, item.data.url);
+      } else if (path.extname(item.data.url) == '.gif') {
+        fs.writeFile(`./downloads/${item.data.title}`, item.data.url);
+      }
+    })
+    fs.writeFileSync(downloadPath, res);
+  })
